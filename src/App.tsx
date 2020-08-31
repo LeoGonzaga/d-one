19b7678c1eb06f88debff3885d2d5143eda8e244
Leo Gonzaga <sacredlotus05@gmail.com>
2020-08-31 20:30:52 -0300

    drawer estilizado e adicionado itens estaticos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./Global/style.css";
 import Dashboard from "./Pages/Dashboard/DashboardScreen";
+import DrawerLeft from "./Components/Drawer";
 
 function App() {
   return (
     <Router>
+      <DrawerLeft />
       <Switch>
         <Route path="/" exact>
-          <Dashboard />
+          {/* <Dashboard /> */}
         </Route>
       </Switch>
     </Router>
---
+++ src/Components/Drawer.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+import Logo from "../Assets/logo.png";
+const Container = styled.div`
+  top: 0px;
+  left: 0px;
+  width: 71px;
+  height: 768px;
+  background: #1a1731 0% 0% no-repeat padding-box;
+`;
+
+const Line = styled.div`
+  width: 71px;
+  height: 3px;
+  background: transparent linear-gradient(90deg, #00e1ff 0%, #117eff 100%) 0% 0%
+    no-repeat padding-box;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  max-width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+const D1Logo = styled.img`
+  width: 40px;
+  height: 30px;
+  padding-top: 35px;
+`;
+
+const ListPages = styled.ul`
+  padding-top: 60px;
+`;
+const ListItem = styled.li`
+  margin-bottom: 10px;
+`;
+const DrawerLeft: React.FC = () => {
+  return (
+    <>
+      <Line></Line>
+      <Container>
+        <Wrapper>
+          <D1Logo src={Logo} />
+          <ListPages>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+          </ListPages>
+
+          <ListPages>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+            <ListItem>
+              <AccessAlarmIcon color="primary" />
+            </ListItem>
+          </ListPages>
+        </Wrapper>
+      </Container>
+    </>
+  );
+};
+
+export default DrawerLeft;
